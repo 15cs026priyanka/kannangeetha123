52c1db1954c364f5820fb12affc750b0f6cc5a59
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-21 15:35:42 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+a=int(input("enter the numbers")
+{
+     int res = ar[0]
+     for (int i=1; i < ar_size; i++):
+        res = res ^ ar[i]
+      return res
+}
+int main()
+{
+     int ar[] = {2, 3, 5, 4, 5, 3, 4}
+     int n = sizeof(ar)/sizeof(ar[0])
+     print"Element occurring once is " << findSingle(ar, n)
+}
